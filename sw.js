f244f0ba3dda471128d332ff61c558fd7350aead
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-02-26 14:05:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-const CACHE = 'oquefazer-v2';
-const FILES = ['/oquefazer/', '/oquefazer/index.html', '/oquefazer/icone.png'];
+const CACHE = 'oquefazer-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
