bdd76ba2ed34737770a8dd5ca553840d6470af99
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-02-26 13:17:31 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'oquefazer-v1';
+const FILES = ['/oquefazer/', '/oquefazer/index.html', '/oquefazer/icone.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
