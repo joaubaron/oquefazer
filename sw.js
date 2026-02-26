be2ab7ba28f795433ff052c8200a5f0f12c40625
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-02-26 16:04:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,10 +8,11 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.map(key => caches.delete(key)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
