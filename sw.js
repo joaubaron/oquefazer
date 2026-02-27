02b3cc1d4690c43abb4e1632b425a020fa689066
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-02-27 10:11:24 -0300

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -1,19 +1,16 @@
-const CACHE = 'oquefazer-v3';
-
 self.addEventListener('install', e => {
-  self.skipWaiting();
+self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    ).then(() => self.clients.claim())
-  );
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.map(key => caches.delete(key)))
+).then(() => self.clients.claim())
+);
 });
 
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e
-.request)));
+if (e.request.method !== 'GET') return;
+e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
