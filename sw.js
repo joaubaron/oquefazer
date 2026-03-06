bfe3437d110171bd6136c2a0ff2567e7e8822da1
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-03-06 09:05:46 -0300

    Update CACHE_VERSION to 1.1.100

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ⚠️ Atualize a versão a cada deploy
-const CACHE_VERSION = '1.1.10';
+const CACHE_VERSION = '1.1.100';
 const CACHE_NAME = `oquefazer-${CACHE_VERSION}`;
 const STATIC_ASSETS = [
 './',
